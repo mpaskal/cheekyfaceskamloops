f0a6f7848318ec31ce23ea68c15f5d4975fed7e3
Marina Paskal <paskalm@rogers.com>
2024-11-04 23:41:09 -0500

    Add script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
+// DOM Elements
+const elements = {
+  copyright: document.querySelector(".copyright"),
+};
 
+function updateCopyright() {
+  const currentYear = new Date().getFullYear();
+  elements.copyright.innerHTML = `${currentYear} Cheeky Faces. All rights reserved.`;
+}
+
+(function () {
+  updateCopyright();
+})();
